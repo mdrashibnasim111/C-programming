d34fbd5e6f5349c8c96f6d5de147da2af9ae2a03
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-28 22:53:03 +0530

    \n ,\t ,\' ,\"

---
+++ specialCharacter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("BISMILLAHIR RAHMANIR RAHIM\n");
+    printf("this is my first code\t");
+    printf("I am learning C programming\t");
+    printf("\"I am very happy to learn C programming\"");
+    getch();
+}
