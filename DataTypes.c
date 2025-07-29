4989925df539d5c133731a57e0f371a4c07f6056
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-30 00:56:52 +0530

    int , %d

--- DataTypes.c
+++ DataTypes.c
@@ -5,5 +5,5 @@ void main()
     int a=50 , b=30 , c;                              // declariation of variables
     c = a + b;                                           //c is in first beacuse, c is based on stack memory and it is runs on (LIFO)last in first out principle
     printf("addition of %d and %d is %d", a, b, c);    // %d is used to print integer values
-    getch();
-}
+    getch();                           
+}                                          
