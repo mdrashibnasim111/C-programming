1ed17b002b1ea9deac9ed7f635921fc0a127b2c8
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-30 01:42:00 +0530

    question

---
+++ Question.c
@@ -0,0 +1,18 @@
+//w.a.p to enter two number and print addition, subtraction, multiplication and division of two numbers.
+#include<stdio.h>
+#include<conio.h>   
+void main()
+{
+    int a,b,c;
+    printf("enter two number");
+    scanf("%d %d",&a,&b);
+    c = a +b;
+    printf("addition of %d and %d is %d",a,b,c);     
+    c=a-b;
+    printf("\nsubtraction of %d and %d is %d",a,b,c);
+    c=a*b;
+    printf("\nmultiplication of %d and %d is %d",a,b,c);
+    c=a/b;
+    printf("\ndivision of %d and %d is %d",a,b,c);
+    getch();
+} 
