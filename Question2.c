c5e57fce6ed39885cbaf070f3ba8179c623f83bd
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-30 02:07:27 +0530

    Question2

---
+++ Question2.c
@@ -0,0 +1,11 @@
+//w.a.p to enter two numbers and print swapping of these two numbers.
+#include <stdio.h>  
+#include<conio.h> 
+void main()
+{
+    int a,b;
+    printf("Enter two numbers\n");
+    scanf("%d %d",&a,&b);
+    printf(" after swapping  a = %d, b = %d\n", b, a);
+    getch();
+}
