cc7d396f83a1e0d5a2835d3569ad2cde6edd6f99
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-30 01:20:14 +0530

    scanf();

--- DataType.c
+++ DataType.c
@@ -4,8 +4,9 @@ void main()
 {    
 int a,b,c;                                     // declaration of variables
     printf("Enter two integers: ");           
-    scanf("%d",&a);                            // input first integer by user
+    scanf("%d",&a);                            // input first integer by user 
     scanf("%d",&b);                            // input second integer by user
+                                             /* we can also write scanf("%d %d",&a,&b); */ 
     c = a + b;
     printf("addition of %d and %d is %d ",a,b,c);
     getch();
