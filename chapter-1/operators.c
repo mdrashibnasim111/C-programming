3462d276cb365bb3fa5d84ca7027d700fa9ae162
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-01 01:33:48 +0530

    operators

---
+++ chapter-1/operators.c
@@ -0,0 +1,17 @@
+//w.a.p to demonstrate operators in C.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d;
+    a=15;
+    b=12;
+    c=10;
+    d=(b<c)==(a<c);               /*(arthmatical + - * / %)  (relational > < <= >= == !=)*/
+    printf("%d",d); 
+    getch();
+}  
+
+// % is used to find remainder
+// == is used to compare two values
+// != is used to check if two values are not equal (opposite of ==)
