40a918355987478d94c53c7e0298f69356a8366e
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-07-31 23:57:55 +0530

    my first code

--- chapter-1/myfirstcode.c
+++ chapter-1/myfirstcode.c
@@ -2,9 +2,6 @@
 #include<conio.h>
 void main()
 {
-    printf("BISMILLAHIR RAHMANIR RAHIM\n");
-    printf("this is my first code\t");
-    printf("I am learning C programming\t");
-    printf("\"I am very happy to learn C programming\"");
+    printf("BISMILLAHIR RAHMANIR RAHIM");
     getch();
 }
