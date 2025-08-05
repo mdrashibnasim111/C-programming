3e1bd3b1fe986119405a17cf6620bcd822185d3a
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-05 23:49:13 +0530

    switch case

--- Switch_case.c
+++ Switch_case.c
@@ -7,7 +7,7 @@ void main()
     scanf("%d", &n);
     switch (n)
     {
-        case 1: printf("you entered one\n");break;
+        case 1: printf("you entered one\n");break;    //break is used to exit the switch case after executing.
         case 2: printf("you entered two\n");break;
         case 3: printf("you entered three\n");break;
         case 4: printf("you entered four\n");break;
