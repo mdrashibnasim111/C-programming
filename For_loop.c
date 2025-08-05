4cabe5a6f6070e6239d70fc266f65a62c3080a2b
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-06 00:36:32 +0530

    For loop

---
+++ For_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    for(i=1; i<=10; i++)                // Initialization, Condition, Increment/Decrement.
+    {
+        printf("%d\n", i);  
+    }
+    getch();
+}
+  //we use FOR-LOOP when the initialization, condition, and increment/decrement are known beforehand.g
