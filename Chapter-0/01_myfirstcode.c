d530c89b7dac2fc845fed2855cd900511589f048
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-09 22:02:53 +0530

    initial commit

---
+++ Chapter-0/01_myfirstcode.c
@@ -0,0 +1,12 @@
+                             /*
+                                Header file which store functions
+                                like printf, scanf, etc.
+                             */
+                               
+#include<stdio.h>                  
+#include<conio.h>
+void main()                                           // main function is the entry point of the program
+{                                                     // opening of main function
+    printf("BISMILLAHIR RAHMANIR RAHIM");
+    getch();
+}                                                    // closing of main function 
