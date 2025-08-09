fc4414fe7a031f164b0eb03442f65457278e8ada
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-09 17:04:29 +0530

    \n , \t , \' , "

--- specialCharacter.c
+++ specialCharacter.c
@@ -2,16 +2,10 @@
 #include<conio.h>
 void main()
 {
-<<<<<<< HEAD:specialCharacter.c
+
     printf("BISMILLAHIR RAHMANIR RAHIM\n");      //we use \n for new line
     printf("this is my first code\t");           //we use \t for tab space
     printf("I am learning C programming\t");
     printf("\"I am very happy to learn C programming\""); //we use \" to print double quotes
-=======
-    printf("BISMILLAHIR RAHMANIR RAHIM\n");
-    printf("this is my first code\t");
-    printf("I am learning C programming\t");
-    printf("\"I am very happy to learn C programming\"");
->>>>>>> babed01 ( \n , \t , \' ,"):chapter-1/specialCharacter.c
     getch();
 }
