dddb011536ffb5ee9d06ad29d4d60e3a51d24eb7
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-13 02:11:31 +0530

    problem

---
+++ Chapter-3/11_problem.c
@@ -0,0 +1,27 @@
+/*
+Calculate income tax paid by an employee to the government as per the slabs mentioned below:
+Income Slab	Tax
+2.5L – 5.0L	    5%
+5.0L – 10.0L	20%
+Above 10.0L	    30%
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    float a ;
+    printf("enter the tax paid(In lakh) :\n");
+    scanf("%f",&a);
+    if(a>=2.5 && a<=5.0)
+    {
+     printf("your tax will be 5%%");
+    }else if(a>5.0 && a<=10.0)
+            {
+            printf("your tax will be 20%%");
+            }else if(a>10.0)
+                {
+                 printf("your tax will be 30%%");
+                }
+return 0;
+}
