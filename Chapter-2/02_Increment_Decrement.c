3f335fba3dee2d1f48b3671eec513d89306c1137
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-12 23:41:27 +0530

    initial commit

---
+++ Chapter-2/02_Increment_Decrement.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5, b,c;
+    b=a++;                    // Post-increment: first assign,then increment   
+    c=--b;                    // Pre-increment: first decrement, then assign
+    a=c++;                    // Post-increment: first assign, then increment
+    b=a--;                    // Post-decrement: first assign, then decrement
+    printf("%d %d %d", a, b, c);
+    getch();
+}
+
+// increment and decrement operators in C++ are only used with variables, not with constants or expressions.
+// The output of this program will be: a=3,b=4,c=5
+
+/*  priority list :-
+    (1)Pre-increment
+    (2)Arithmatic
+    (3)Assign
+    (4)Post-increment
+*/
