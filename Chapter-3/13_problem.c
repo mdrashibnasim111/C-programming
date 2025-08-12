d063e558bd0c301d10025cd90abadc04a766e3cf
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-13 03:18:51 +0530

    problem

---
+++ Chapter-3/13_problem.c
@@ -0,0 +1,18 @@
+// Write a program to find whether a year entered by the user is a leap year or not.
+// Take year as an input from the user.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter the year :");
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+    printf("This is a leap year");
+    }
+    else
+    printf("This is not a leap year");
+    return 0;
+}
