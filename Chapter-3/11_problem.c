9f63dab2357d2f9d8658d20646282bd5104813a1
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-13 02:18:04 +0530

    problem

--- Chapter-3/11_problem.c
+++ Chapter-3/11_problem.c
@@ -13,15 +13,18 @@ int main ()
     float a ;
     printf("enter the tax paid(In lakh) :\n");
     scanf("%f",&a);
-    if(a>=2.5 && a<=5.0)
+    if(a<2.5)
     {
-     printf("your tax will be 5%%");
-    }else if(a>5.0 && a<=10.0)
-            {
-            printf("your tax will be 20%%");
-            }else if(a>10.0)
-                {
-                 printf("your tax will be 30%%");
-                }
+    printf("there is no tax");
+    }else if(a>=2.5 && a<=5.0)                   //&& is used to indicate range
+           {
+           printf("your tax will be 5%%");
+           }else if(a>5.0 && a<=10.0)           //else-if condition (also called multiple if-else, ladders if-else)
+                  {
+                  printf("your tax will be 20%%");
+                  }else if(a>10.0)
+                          {
+                          printf("your tax will be 30%%");
+                          }
 return 0;
 }
