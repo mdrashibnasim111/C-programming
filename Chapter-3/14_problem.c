303948dcd354dd0c40a8eb5b8c4274dc9697b0d6
mdrashibnasim111 <mdrashibnasim111@gmail.com>
2025-08-13 03:39:06 +0530

    problem

---
+++ Chapter-3/14_problem.c
@@ -0,0 +1,28 @@
+//Write a program to determine whether a character entered by the user is lowercase or not.
+#include <stdio.h>
+
+int main()
+ {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <= 'z')                             /*
+                                     In C, every character is stored as an ASCII value (a number behind the scenes).
+                                                                              For example:
+                                                                               'a' → 97
+                                                                               'z' → 122
+                                                                               'A' → 65
+                                                                               'Z' → 90
+                                                            */
+    {                               //This checks if the character is in the range from 'a' to 'z'
+        printf("The character '%c' is lowercase.\n", ch);
+    } 
+    else 
+    {
+        printf("The character '%c' is NOT lowercase.\n", ch);
+    }
+
+    return 0;
+}
